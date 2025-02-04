7d02fc23db1335d8d0a35b44cc71c63bfc3f853a
iamcta <iamchristaylor@gmail.com>
2025-02-04 12:40:23 -0600

    Set up basic api calls login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,11 +1,35 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ApiService } from '../../services/api.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  loginForm: FormGroup;
+  errorMessage: string = '';
 
+  constructor(private fb: FormBuilder, private apiService: ApiService, private router: Router) {
+    this.loginForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      this.apiService.login(this.loginForm.value).subscribe({
+        next: (response) => {
+          localStorage.setItem('token', response.token);
+          this.router.navigate(['/dashboard']);
+        },
+        error: (err) => {
+          this.errorMessage = 'Invalid credentials. Please try again.';
+        }
+      });
+    }
+  }
 }
